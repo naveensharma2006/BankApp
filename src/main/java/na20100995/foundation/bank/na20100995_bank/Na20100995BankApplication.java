d67f3fd6347c69f82c87e19c41bb54db23686e82
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-08-07 15:03:50 +0530

    Comments

--- src/main/java/na20100995/foundation/bank/na20100995_bank/Na20100995BankApplication.java
+++ src/main/java/na20100995/foundation/bank/na20100995_bank/Na20100995BankApplication.java
@@ -3,6 +3,12 @@ package na20100995.foundation.bank.na20100995_bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot Class
+ * 
+ * @author naveensharma
+ *
+ */
 @SpringBootApplication
 public class Na20100995BankApplication {
 
