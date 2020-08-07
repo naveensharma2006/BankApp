91ff472a2636765bffaeec939e785726de5faf23
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-08-07 16:05:04 +0530

    tt

--- src/main/java/na20100995/foundation/bank/na20100995_bank/service/AccountsService.java
+++ src/main/java/na20100995/foundation/bank/na20100995_bank/service/AccountsService.java
@@ -3,6 +3,10 @@ package na20100995.foundation.bank.na20100995_bank.service;
 import java.util.List;
 import na20100995.foundation.bank.na20100995_bank.entity.Account;
 
+/**
+ * @author naveensharma
+ *
+ */
 public interface AccountsService {
 
 	public List<Account> getAllAccounts();
