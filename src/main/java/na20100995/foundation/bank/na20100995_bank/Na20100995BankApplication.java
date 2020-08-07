3e95c7db0a452e2c945f6eb1386b9437e6ac5b8c
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-08-07 14:58:30 +0530

    First commit

---
+++ src/main/java/na20100995/foundation/bank/na20100995_bank/Na20100995BankApplication.java
@@ -0,0 +1,13 @@
+package na20100995.foundation.bank.na20100995_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Na20100995BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Na20100995BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/na20100995/foundation/bank/na20100995_bank/Na20100995BankApplicationTests.java
@@ -0,0 +1,13 @@
+package na20100995.foundation.bank.na20100995_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Na20100995BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
